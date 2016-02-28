0e6e906321e96ebdb2efd9d36b147f67b1372c2d
willchen90 <willchen90@gmail.com>
2016-02-27 17:08:40 -0800

    implement media + setup gulp autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,15 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
  
 gulp.task('sass', function () {
   return gulp.src('./styles/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
     .pipe(gulp.dest('./dist/'));
 });
  
